5b4dfd3d794306e859f9555841cd3b948c53e178
Mohamed Youssef <mohamed.youssef@gcg.ae>
2025-04-14 12:16:02 +0400

    update the final design

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -16,80 +16,30 @@ $(document).ready(function(){
 			}, 1500);
 			return false;
 		});
-	
-	
-	// 2. slick carousel
 
-	    $(".testimonial-carousel").slick({
-	        infinite: true,
-	        centerMode: true,
-	        autoplay:true,
-	        slidesToShow: 5,
-	        slidesToScroll: 3,
-	        autoplaySpeed:1500,
-	        // the magic
-			responsive: [
-				{
-
-					breakpoint:1440,
-					settings: {
-					slidesToShow:3
-					}
-
-				},
-				{
-
-					breakpoint: 1024,
-					settings: {
-					slidesToShow:2,
-					
-					}
-
-				}, 
-				{
-
-					breakpoint:991,
-					settings: {
-					slidesToShow:2,
-					centerMode:false,
-					}
-
-				},
-				{
-
-					breakpoint:767,
-					settings: {
-					slidesToShow:1,
-					}
-
-				}
-			]
-	    });
-
-
-
-    // 3. welcome animation support
-
-        $(window).load(function(){
-        	$(".welcome-hero-txt h2,.welcome-hero-txt p").removeClass("animated fadeInUp").css({'opacity':'0'});
-            $(".welcome-hero-serch-box").removeClass("animated fadeInDown").css({'opacity':'0'});
-        });
-
-        $(window).load(function(){
-        	$(".welcome-hero-txt h2,.welcome-hero-txt p").addClass("animated fadeInUp").css({'opacity':'0'});
-            $(".welcome-hero-serch-box").addClass("animated fadeInDown").css({'opacity':'0'});
-        });
-
-	// 4. feather icon
-
-		feather.replace();
-
-	// 5. counter
-		$(window).on('load', function(){	
-			$('.counter').counterUp({
-				delay: 10,
-				time: 3000
-			});	
-		});
-
+    // client section owl carousel
+    $(".client_owl-carousel").owlCarousel({
+        loop: true,
+        margin: 20,
+        dots: false,
+        nav: true,
+        navText: [],
+        autoplay: true,
+        autoplayHoverPause: true,
+        navText: [
+            '<i class="fa fa-angle-left" aria-hidden="true"></i>',
+            '<i class="fa fa-angle-right" aria-hidden="true"></i>'
+        ],
+        responsive: {
+            0: {
+                items: 1
+            },
+            600: {
+                items: 2
+            },
+            1000: {
+                items: 2
+            }
+        }
+    });
 });
